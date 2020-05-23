d02250d0944601d76a6eb2f7b6f85eded477c20a
serik.shaikamalov <serik.shaikamalov@ntpayments.com>
2020-05-23 21:51:31 +0600

    added http adapter

--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,18 @@
 import 'package:flutter/material.dart';
 import 'package:namazapp/core/constants/pages-header.dart';
 import 'package:namazapp/core/constants/routes.dart';
+import 'package:namazapp/core/service/http.dart';
 import 'package:namazapp/router.dart';
 import 'locator.dart' as di;
 
 void main() async {
+  // Inialize dependecies
   await di.init();
+
+  // Run interceptors
+  setupInterceptors();
+
+  // Start app
   runApp(MyApp());
 }
 
@@ -14,6 +21,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       title: PagesHeader.home,
+      debugShowCheckedModeBanner: false,
       initialRoute: Routes.taharatPage,
       onGenerateRoute: Router.generateRoute,
     );
