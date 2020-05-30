42e5268d0939b1ac8d41552cc527d3a9e9383fbb
serik.shaikamalov <serik.shaikamalov@ntpayments.com>
2020-05-30 22:53:19 +0600

    namaz parts added

---
+++ lib/features/namaz/data/parts/fatiha_kausar/fatiha-kausar-man-part.dart
@@ -0,0 +1,6 @@
+import 'package:namazapp/features/namaz/data/parts/fatiha_asr/fatiha-asr-part.dart';
+import 'package:namazapp/features/namaz/data/parts/fatiha_kausar/fatiha-kausar-part.dart';
+
+class FatihaKausarManPart extends FatihaKausarPart {
+  String image = 'assets/images/namaz/man/kiyam.png';
+}
